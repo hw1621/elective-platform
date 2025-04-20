4cbcb798cabd97ec9e36bf85a6b59487e6460b10
hw1621 <wanghaoran030401@163.com>
2025-04-20 21:34:01 +0100

    remove type in module_instance

---
+++ prisma/migrations/20250420203332_remove/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `type` on the `module_instance` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "module_instance" DROP COLUMN "type";
