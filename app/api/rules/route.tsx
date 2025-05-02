4cdeaed39e8333d4f16199c705d31901f9240f5f
hw1621 <wanghaoran030401@163.com>
2025-05-02 02:05:00 +0100

    run eslint

--- app/api/rules/route.tsx
+++ app/api/rules/route.tsx
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
 
 const prisma = new PrismaClient();
