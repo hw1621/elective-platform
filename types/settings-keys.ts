b9c24f0ccb119a5f59f5f2591b8bb7c58f7f4fc4
hw1621 <wanghaoran030401@163.com>
2025-05-21 02:50:48 +0100

    create settings-keys type

---
+++ types/settings-keys.ts
@@ -0,0 +1,10 @@
+export const SettingKeys = {
+    ENABLE_SIT_IN: "enable_sit_in",
+    FIRST_ROUND_START_DATE: "first_round_start_date",
+    FIRST_ROUND_END_DATE: "first_round_end_date",
+    SECOND_ROUND_START_DATE: "second_round_start_date",
+    SECOND_ROUND_END_DATE: "second_round_end_date",
+} as const
+
+export type SettingKeys = keyof typeof SettingKeys;
+export type SettingValues = typeof SettingKeys[SettingKeys];
