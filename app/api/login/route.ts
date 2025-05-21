2528557dce93b483c252061b3b137ea8f2bc2f28
hw1621 <wanghaoran030401@163.com>
2025-05-21 02:52:29 +0100

    remove unused request in login GET

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -63,7 +63,7 @@ export async function POST(request: Request) {
 }
 
 //Get student info
-export async function GET(request: Request) {
+export async function GET() {
   const session = await getServerSession(authOptions)
   
   if (!session) {
