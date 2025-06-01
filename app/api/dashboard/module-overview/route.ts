3daa31cebb6af10ecf45dda15915550bbab98c37
hw1621 <wanghaoran030401@163.com>
2025-06-01 01:12:31 +0100

    filter out compulsory modules in modules dashboard

--- app/api/dashboard/module-overview/route.ts
+++ app/api/dashboard/module-overview/route.ts
@@ -15,6 +15,28 @@ export async function GET(req: NextRequest) {
     }
 
     try {
+        const compulsoryModules = await prisma.module_group_mapping.findMany({
+            where: {
+                deleted_at: null,
+                module_group: {
+                    deleted_at: null,
+                    program_id: parseInt(programId),
+                    rules: {
+                        some: {
+                            deleted_at: null,
+                            is_compulsory: true,
+                        }
+                    }
+                }
+            },
+            select: {
+                module_id: true,
+            }
+        });
+
+        console.log(compulsoryModules)
+        const excludedModuleIds = new Set(compulsoryModules.map(m => m.module_id));
+
         const groupedModules = await prisma.module_selection_result.groupBy({
             by: ['module_id', 'type'],
             where: {
@@ -22,32 +44,34 @@ export async function GET(req: NextRequest) {
                 student: {
                     program_id: parseInt(programId),
                     deleted_at: null
+                },
+                module_id: {
+                    notIn: Array.from(excludedModuleIds)
                 }
             },
             _count: {
                 _all: true,
             }
         })
+        console.log(groupedModules)
 
-        //Create module id maps for later modules information fetch
+        //Find the non-compulsory module information 
+        const modules = await prisma.module.findMany({
+            where: { id: { in: Array.from(new Set(groupedModules.map(g => g.module_id))) } },
+            select: { id: true, title: true }
+        });
+
+        const moduleMap = Object.fromEntries(modules.map(m => [m.id, m.title]));
+
         //Create module selection status which has structure: { 1 : { "SITIN": 1, "REGISTER": 2 }, 2: { "SITIN": 4, "REGISTER": 5 }}
         const moduleStats: Record<number, Record<string, number>> = {}
-        const moduleIdSet = new Set<number>();
         for (const entry of groupedModules) {
             const { module_id, type, _count } = entry;
             if (!moduleStats[module_id]) {
                 moduleStats[module_id] = {}
             }
             moduleStats[module_id][type] = _count._all;
-            moduleIdSet.add(module_id);
         }
-
-        const modules = await prisma.module.findMany({
-          where: { id: { in: Array.from(moduleIdSet) } },
-          select: { id: true, title: true }
-        });
-
-        const moduleMap = Object.fromEntries(modules.map(m => [m.id, m.title]))
 
         const result = Object.entries(moduleStats).map(([moduleId, counts]) => {
             const total = Object.values(counts).reduce((sum, count) => sum + count, 0);
