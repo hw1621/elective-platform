1b8f4186ca24f1c1c85de37ac468da875ad95cb8
hw1621 <wanghaoran030401@163.com>
2025-06-20 21:49:54 +0100

    refine dashboard

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -52,6 +52,66 @@ export default function DashboardPage() {
   //All programs
   const [programs, setPrograms] = useState<Program[]>([]);
 
+  // useEffect(() => {
+  //   const mockData: StudentRegistrationDataItem[] = [
+  //     { program_name: "Finance", COMPLETE: 45, IN_PROGRESS: 7, NOT_STARTED: 2 },
+  //     { program_name: "FinTech", COMPLETE: 25, IN_PROGRESS: 8, NOT_STARTED: 1 },
+  //     { program_name: "IWM", COMPLETE: 33, IN_PROGRESS: 10, NOT_STARTED: 5 },
+  //     { program_name: "F&A", COMPLETE: 24, IN_PROGRESS: 7, NOT_STARTED: 3 },
+  //     { program_name: "RMFE", COMPLETE: 19, IN_PROGRESS: 2, NOT_STARTED: 0 },
+  //     { program_name: "Exchange", COMPLETE: 10, IN_PROGRESS: 0, NOT_STARTED: 1 },
+  //   ];
+  //   setStudentRegistrationData(mockData);
+  // }, []);
+
+  // useEffect(() => {
+  //   const mockModules: ModuleSelectionItem[] = [
+  //     {
+  //       module_id: 6,
+  //       title: "Sustainable Finance",
+  //       counts: { CREDIT: 40, SITIN: 0 },
+  //       total: 40,
+  //     },
+  //     {
+  //       module_id: 7,
+  //       title: "Financial Modelling",
+  //       counts: { CREDIT: 34, SITIN: 0 },
+  //       total: 34,
+  //     },
+  //     {
+  //       module_id: 5,
+  //       title: "Python for Finance",
+  //       counts: { CREDIT: 28, SITIN: 4 },
+  //       total: 28,
+  //     },
+  //     {
+  //       module_id: 1,
+  //       title: "Big Data, AI and Machine Learning",
+  //       counts: { CREDIT: 18, SITIN: 3 },
+  //       total: 21,
+  //     },
+  //     {
+  //       module_id: 2,
+  //       title: "Consumer Behaviour",
+  //       counts: { CREDIT: 9, SITIN: 6 },
+  //       total: 15,
+  //     },
+  //     {
+  //       module_id: 3,
+  //       title: "Brand Experience Design",
+  //       counts: { CREDIT: 7, SITIN: 4 },
+  //       total: 11,
+  //     },
+  //     {
+  //       module_id: 4,
+  //       title: "Business Analytics",
+  //       counts: { CREDIT: 7, SITIN: 2 },
+  //       total: 9,
+  //     },
+  //   ];
+  //   setModuleSelectionData(mockModules);
+  // }, [selectedProgramId]);
+  
   useEffect(() => {
     const fecthPrograms = async () => {
       try {
@@ -136,7 +196,7 @@ export default function DashboardPage() {
                 margin={{ top: 20, right: 20, left: 20, bottom: 20 }}
               >
                 <XAxis dataKey="program_name" fontSize={15} />
-                <YAxis allowDecimals={false} tickCount={6} />
+                <YAxis allowDecimals={false} tickCount={10} />
                 <Tooltip />
                 <Legend verticalAlign="top" align="right" />
                 <Bar dataKey={SelectionStatus.COMPLETE} stackId="a" name="Complete" fill="#16a34a" barSize={60} />
@@ -193,17 +253,24 @@ export default function DashboardPage() {
                 }))}
                 layout="vertical"
                 barCategoryGap={3}
+                barGap={0}
                 margin={{ top: 20, right: 40, bottom: 40 }}
               >
-                <XAxis type="number" allowDecimals={false} tickCount={6}/>
-                <YAxis dataKey="title" type="category" width={310} />
-                <Legend />
-                <Bar dataKey={RegisterLevel.CREDIT} stackId="a" name="Credit" fill="#16a34a" barSize={25}/>
+                <XAxis type="number" allowDecimals={false} tickCount={10}/>
+                <YAxis dataKey="title" type="category" width={300} />
+                <Tooltip formatter={(value: number, name: string) => [`${value} students`, name]} />
+                <Legend
+                  verticalAlign="top"
+                  align="right"
+                  layout="vertical"
+                  wrapperStyle={{ right: 0, top: 0 }}
+                />
+                <Bar dataKey={RegisterLevel.CREDIT} stackId="a" name="Credit" fill="#16a34a" barSize={25} />
                 <Bar dataKey={RegisterLevel.SITIN} stackId="a" name="Sit-in" fill="#9ca3af">
                   <LabelList
                     dataKey="total"
                     position="right"
-                    style={{ fill: '#000', fontSize: 14, fontWeight: 600 }}
+                    style={{ fill: '#000', fontSize: 13, fontWeight: 500 }}
                   />
                 </Bar>
               </BarChart>
